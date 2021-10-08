837d6c764da3f337e0b6db790d90561fca3e4fc0
bmankowitz <benjamin.mankowitz@gmail.com>
2021-10-07 22:42:09 -0400

    Initial commit

---
+++ com3800/stage1/src/main/java/edu/yu/cs/com3800/stage1/ClientImpl.java
@@ -0,0 +1,20 @@
+package main.java.edu.yu.cs.com3800.stage1;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class ClientImpl implements Client {
+    public ClientImpl(String hostName, int hostPort) throws MalformedURLException {
+
+    }
+
+    @Override
+    public void sendCompileAndRunRequest(String src) throws IOException {
+
+    }
+
+    @Override
+    public Response getResponse() throws IOException {
+        return null;
+    }
+}
---
+++ com3800/stage1/src/test/java/edu/yu/cs/com3800/stage1/Stage1Test.java
@@ -0,0 +1,14 @@
+package edu.yu.cs.com3800.stage1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Stage1Test {
+
+    @Test
+    public void test1(){
+        assertEquals(1,1);
+    }
+}
